f2de68b19a527a89d49d4288201d2a88954edee2
hieucckha <conpasslaconheo@gmail.com>
2022-12-06 09:11:47 +0700

    First push to github

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  bracketSpacing: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  vueIndentScriptAndStyle: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  twigPrintWidth: 100,
+  twigMultiTags: ['html_element,end_html_element', 'with,endwith'],
+};
