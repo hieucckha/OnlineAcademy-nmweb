7b8fcc1919fb185f27ce0095fa1fb92084a324c8
Suong Vo <vothanhsuong123@gmail.com>
2022-12-30 21:40:00 +0700

    Add category and user service

--- models/Account.model.js
+++ models/Account.model.js
@@ -1,18 +1,45 @@
-export default {
-  getInfo() {
-    const Info = {
-      Id: 1,
-      FirstName: 'Bảo',
-      LastName: 'Hoàng',
-      Caption: '',
-      Biography: '',
-      Language: '',
-      EmailContant: '',
-      PhoneNumber: '',
-      Avatar: '',
-      Website: '',
-      Language: '',
-    };
-    return Info;
-  },
-};
+export default class User {
+  constructor(
+    userId,
+    email,
+    phone,
+    password,
+    type,
+    firstName,
+    lastName,
+    avatar,
+    caption,
+    bio,
+    website,
+    language
+  ) {
+    this.userId = userId;
+    this.email = email;
+    this.phone = phone;
+    this.password = password;
+    this.type = type;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.avatar = avatar;
+    this.caption = caption;
+    this.bio = bio;
+    this.website = website;
+    this.language = language;
+  }
+  // getInfo() {
+  //   const Info = {
+  //     Id: 1,
+  //     FirstName: 'Bảo',
+  //     LastName: 'Hoàng',
+  //     Caption: '',
+  //     Biography: '',
+  //     Language: '',
+  //     EmailContant: '',
+  //     PhoneNumber: '',
+  //     Avatar: '',
+  //     Website: '',
+  //     Language: '',
+  //   };
+  //   return Info;
+  // },
+}
--- services/courses.service.js
+++ services/courses.service.js
@@ -1,2 +1,6 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import db from '../utils/db.js';
-export default {};
+import category from '../models/category.models.js';
+
+module.exports = {};
