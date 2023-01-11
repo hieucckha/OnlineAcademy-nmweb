572e6059335ac62df1f8389cc83857aec4640e58
baokyo002 <74838783+baokyo002@users.noreply.github.com>
2023-01-11 14:55:24 +0700

    Update teacher

--- routes/teachers.route.js
+++ routes/teachers.route.js
@@ -22,17 +22,26 @@ router.get('/new/course', async (req, res) => {
     categoryList: categoryList
   });
 });
-router.get('/edit/course', (req, res) => {
-  res.render('teacher/showAllCourse', {
-    courseList: CourseList,
-    layout: 'teacher.hbs'
-  });
-});
-router.get('/edit/course/:id', (req, res) => {
-  const Course = courseModel.CourseDetail();
-  console.log(Course);
+router.get('/edit/course/', async (req, res) => {
+  const courseId = req.query.courseId;
+  const course = await coursesService.getFullCourse(courseId);
+  const instructor = await userService.getById(course.createBy);
+  let numLecture = 0;
+
+  for (let i = 0; i < course.sectionList.length; i++) {
+    if(course.sectionList[i].listLecture !== null)
+      for (let j = 0; j < course.sectionList[i].listLecture.length; j++) {
+        numLecture += 1;
+      }
+  }
+  if (course === null) {
+    return res.redirect('/');
+  }
+  course.image = '/'+course.image;
   res.render('teacher/editCourse', {
-    course: Course,
+    course: course,
+    instructor: instructor,
+    numLecture: numLecture,
     layout: 'teacher.hbs'
   });
 });
@@ -43,7 +52,39 @@ router.get('/edit/profile/', (req, res)=>{
       }
       );
 })
+router.get('/course/mycourse', async (req,res)=>{
+  const teacherID = 'd172436b-5020-4b34-8827-6ebd041d5474';
+  const page = Number(req.query.page) || 1;
+  const list = await coursesService.getAllCourseTeacher(teacherID, page) || [];
+  let maxPage = 1;
+  let countList = await coursesService.getAllCourseTeacher(teacherID, maxPage);
 
+  while (countList !== null){
+    countList = await coursesService.getAllCourseTeacher(teacherID, maxPage);;
+    maxPage++;
+  }
+  maxPage--;
+  console.log(maxPage);
+  let pageList;
+  pageList = [];
+  if (maxPage === 2) pageList.push({value: page, isActive: true});
+  else if (maxPage === 3 && page === 1) pageList.push({value: page, isActive: true},{value: page+1});
+  else if (maxPage === 3 && page === 2) pageList.push({value: page -1},{value: page, isActive: true});
+  else if (page === 1) pageList.push({value: page, isActive: true},{value: page+1},{value: page+2});
+  else if (list.length === 0) pageList.push({value: page-2},{value: page-1},{value: page, isActive: true})
+  else pageList.push({value: page-1},{value: page, isActive: true},{value: page+1});
+
+  list.forEach(member=>{
+    member.image = '/' + member.image;
+  })
+  res.render('teacher/showAllCourse', {
+    myCourses: list,
+    pageList: pageList,
+    maxPage: maxPage,
+    empty: list === null,
+    layout: 'teacher.hbs'
+  });
+});
 
 router.post('/new/course', async (req, res)=>{
   const teacherID = 'd172436b-5020-4b34-8827-6ebd041d5474';
